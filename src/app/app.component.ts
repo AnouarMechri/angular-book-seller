d405caf57178f437cbed4a001aa59ed2a495a86b
anouar mechri <anouar.mechri@polytechnicien.tn>
2022-01-06 11:02:56 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {user} from "./models/user.model";
+import {AuthenticationService} from "./services/authentication.service";
+import {Router} from "@angular/router";
+import {Role} from "./models/role.enum";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-bookSeller';
+  currentUser: user = new user;
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+    this.authenticationService.currentUser.subscribe(data => {
+      this.currentUser = data;
+    });
+  }
+
+  isAdmin() {
+    return this.currentUser?.role === Role.ADMIN;
+  }
+
+  logOut() {
+    this.authenticationService.logOut();
+    this.router.navigate(['/login']);
+  }
+
 }
